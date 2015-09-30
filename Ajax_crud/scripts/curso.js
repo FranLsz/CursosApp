431f19595f426c4a874851291141a290df1e6fc3
Fran <fran@mail.com>
2015-09-30 14:24:29 +0200

    added

---
+++ Ajax_crud/scripts/curso.js
@@ -0,0 +1,76 @@
+﻿var url = "https://alumnoscurso.azure-mobile.net/Tables/Curso/";
+
+function guardarDatos() {
+    var obj = {
+        nombre: document.getElementById("add_nombre").value,
+        duracion: document.getElementById("add_duracion").value,
+    };
+
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("post", url);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.onreadystatechange = function () {
+        alert();
+        if (ajax.readyState == 4) {
+            if (ajax.status >= 200 && ajax.status < 300) {
+                obtenerDatos();
+            } else {
+                alert();
+            }
+        }
+    }
+    //se transforma a un JSON en texto
+    var data = JSON.stringify(obj);
+    ajax.send(data);
+
+}
+
+
+
+
+function obtenerDatos() {
+    var ajax = new XMLHttpRequest();
+
+    //inicia la comunicacion
+    ajax.open("get", url);
+
+    //cuando cambie el estado ejecuta la funcion (res = respuesta)
+    ajax.onreadystatechange = function () {
+
+        if (ajax.readyState == 4) {
+
+            // entre 200 y 300 respuesta  de exito
+            if (ajax.status >= 200 && ajax.status < 300) {
+
+                var data = JSON.parse(ajax.responseText);
+                var salida = "<table>";
+                for (var i = 0; i < data.length; i++) {
+                    salida += "<tr>";
+                    salida += "<td>" + data[i].nombre + "</td>";
+                    salida += "<td>" + data[i].duracion + "</td>";
+                    salida += "</tr>";
+
+
+                }
+
+                salida += "</table>";
+                document.getElementById("cursos").innerHTML = salida;
+
+            } else {
+                alert("Error en la peticion");
+                console.log(ajax.error);
+            }
+
+
+        }
+
+    };
+
+    ajax.send(null);
+}
+
+
+obtenerDatos();
+
+document.getElementById("btnAdd").onclick = guardarDatos;
